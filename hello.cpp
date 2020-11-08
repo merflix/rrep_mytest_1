5055e8bbbffb9bb6fa455bb6f3635f9b0fc79c34
meichen <merflix@gmail.com>
2020-11-08 18:11:01 +0100

    end of execution instead end.

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main(int argc, char* argv[]){
 	std::cout << "argv[0]: " << argv[0] << std::endl;
-	std::cout << "end. " << std::endl;	
+	std::cout << "end execution. " << std::endl;	
 	return 0;
 }
